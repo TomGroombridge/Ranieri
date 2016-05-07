23e9c3c1175ac1139beb950ca7de3e7c5037f565
Tom Groombridge <groombridgetom@gmail.com>
2016-05-07 12:00:04 +0100

    added about us static page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'interested_users#new'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'about_us' => 'static#about_us'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
